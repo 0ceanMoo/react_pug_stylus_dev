e4ca37a3843ce3bfdaeb9b147541258535c0b1ba
Wataru <wakutsu@d-r.co.jp>
2018-07-29 00:36:22 +0900

    間違ってcommitしちゃったので戻す

--- assets/jsx/app.jsx
+++ assets/jsx/app.jsx
@@ -3,17 +3,15 @@ import css from '../css/app.styl'
 import React from "react"
 import {render} from "react-dom"
 
-class TextInput extends React.Component {
+class App extends React.Component {
   render() {
     return(
-      <div>
-        JSX：<input type="text" defaultValue="入力欄" />
-      </div>
+      <p> Hello React!</p>
     )
   }
 }
 
 render(
-  <TextInput/>,
+  <App/>,
   document.getElementById('app')
 );
